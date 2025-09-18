422a9f9433f19b504876a6d41e982b1134a28181
harivigneshs-pro <harivigneshs002@gmail.com>
2025-09-19 00:57:51 +0530

    Update ComparableEx1.java

--- src/III_Unit/Comparable_Comparator/ComparableEx1.java
+++ src/III_Unit/Comparable_Comparator/ComparableEx1.java
@@ -19,8 +19,8 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-// class Student implements Comparable<Student> {
-class Student {
+class Student implements Comparable<Student> {
+//class Student {
     int age;
     String name;
 
